30510ca0ce9e9d6cb04241eb17fca62b2e521f57
MatíasJMaranga <marangamatias@gmail.com>
2024-02-27 16:36:45 -0300

    jumbotron btn border dark added on hover

--- TemplateLand&Carr/assets/js/index.js
+++ TemplateLand&Carr/assets/js/index.js
@@ -3,40 +3,41 @@ const whappImg = document.getElementById('img-whapp');
 const whappImgBlack = document.getElementById('img-whapp-black');
 
 btnJumbo.onmouseover = () => {
-    if(whappImg.classList.contains('show') && whappImgBlack.classList.contains('hide')) {
-    whappImg.classList.remove('show');
-    whappImg.classList.add('hide');
-    whappImgBlack.classList.remove('hide');
-    whappImgBlack.classList.add('show');
-    } 
+    if (whappImg.classList.contains('show') && whappImgBlack.classList.contains('hide')) {
+        whappImg.classList.remove('show');
+        whappImg.classList.add('hide');
+        whappImgBlack.classList.remove('hide');
+        whappImgBlack.classList.add('show');
+    }
 }
 btnJumbo.onmouseout = () => {
-    if(whappImg.classList.contains('hide') && whappImgBlack.classList.contains('show')) {
-    whappImg.classList.remove('hide');
-    whappImg.classList.add('show');
-    whappImgBlack.classList.remove('show');
-    whappImgBlack.classList.add('hide');
-    } 
+    if (whappImg.classList.contains('hide') && whappImgBlack.classList.contains('show')) {
+        whappImg.classList.remove('hide');
+        whappImg.classList.add('show');
+        whappImgBlack.classList.remove('show');
+        whappImgBlack.classList.add('hide');
+    }
 }
 const btnJumbo1 = document.getElementById('jumbotron-btn1');
 const whappImg1 = document.getElementById('img-whapp1');
 const whappImgBlack1 = document.getElementById('img-whapp-black1');
 
 btnJumbo1.onmouseover = () => {
-    if(whappImg1.classList.contains('show') && whappImgBlack1.classList.contains('hide')) {
-    whappImg1.classList.remove('show');
-    whappImg1.classList.add('hide');
-    whappImgBlack1.classList.remove('hide');
-    whappImgBlack1.classList.add('show');
-    } 
+    if (whappImg1.classList.contains('show') && whappImgBlack1.classList.contains('hide')) {
+        whappImg1.classList.remove('show');
+        whappImg1.classList.add('hide');
+        whappImgBlack1.classList.remove('hide');
+        whappImgBlack1.classList.add('show');
+        btnJumbo1.classList.add('border', 'border-dark')
+    }
 }
 btnJumbo1.onmouseout = () => {
-    if(whappImg1.classList.contains('hide') && whappImgBlack1.classList.contains('show')) {
-    whappImg1.classList.remove('hide');
-    whappImg1.classList.add('show');
-    whappImgBlack1.classList.remove('show');
-    whappImgBlack1.classList.add('hide');
-    } 
+    if (whappImg1.classList.contains('hide') && whappImgBlack1.classList.contains('show')) {
+        whappImg1.classList.remove('hide');
+        whappImg1.classList.add('show');
+        whappImgBlack1.classList.remove('show');
+        whappImgBlack1.classList.add('hide');
+    }
 }
 
 
